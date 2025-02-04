edc116bb7c60f8c29a873aec489efd9d6a490955
i.molochnikov <imoldev@yandex.ru>
2025-02-04 22:48:37 +0300

    compose start

--- Makefile
+++ Makefile
@@ -3,3 +3,6 @@ up:
 
 down:
 	docker compose down --remove-orphans
+
+cli:
+	docker compose run --rm php-fpm  bash	
--- www/public/index.php
+++ www/public/index.php
@@ -1,19 +1,3 @@
-
-<pre>
 <?php
-var_export($_SERVER);
-?>
-</pre>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>LinuxPhp</title>
-</head>
-<body>
-    <h1>Hello from Php!</h1>
-    
-</body>
-</html>
+phpinfo();
